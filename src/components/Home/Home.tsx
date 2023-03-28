c8aa4eb8530836e7325445ef158bdc19b4989c20
bilguun111101 <Mason@Masons-MacBook-Pro.local>
2023-03-29 00:25:19 +0800

    Added some file

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,18 @@
+import { url } from 'inspector';
+import React from 'react'
+// import home from "assets/home.png";
+import { home, play } from '../ImageUrls';
+// import styled from "styled-components";
+
+export const Home = () => {
+  return (
+    <div 
+      className="min-h-screen bg-cover"
+      style={{
+        // backgroundImage: uri,
+      }}
+    >
+      {/* <img src={home} alt="" /> */}
+    </div>
+  )
+}
